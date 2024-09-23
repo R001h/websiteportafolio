86abed6f6e3d2a7108ee5c02e176e93c6e697fbc
R001h <rachidortega@fwdcostarica.com>
2024-09-23 16:14:20 -0600

    commit

--- websitePortafolio/src/pages/AdminUtilities.jsx
+++ websitePortafolio/src/pages/AdminUtilities.jsx
@@ -7,6 +7,7 @@ import ConsultHistory from '../components/ConsultHistory';
 import ToolRegForm from '../components/ToolRegForm';
 import '../styles/AdminUtilities.css';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function AdminUtilities() {
   const navigate = useNavigate();
@@ -28,10 +29,12 @@ function AdminUtilities() {
         <div className='taskbox'><ConsultHistory /></div>
       </div>
       <ToolRegForm />
-      <br /><br />
-
-      <Link to="/AdminShop">Admin Shop</Link>
-      <Link to="/AdminUtilities">AdminTool</Link>
+      <br />
+      
+      <button><Link to="/AdminShop">Admin Shop</Link></button> 
+      <br />
+      <button><Link to="/AdminUtilities">AdminTool</Link></button>
+      
       
 
       <Footer />
